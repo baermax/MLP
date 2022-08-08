a4f46887ce1b1a36b3d2988e2c9724663f3c9c6b
adminatgithub <58471780+adminatgithub@users.noreply.github.com>
2022-08-08 09:36:46 +0200

    Create layers.py

---
+++ layers.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+class FullyConnectedLayer:
+  def __init__(self, inputs, outputs):
+    self.inputs = inputs
+    self.outputs = outputs
+    self.weight = np.random.rand(outputs, inputs)
+    self.bias = np.zeros((outputs, 1))
+    self.x = None
+    self.y = None
+    self.trainable = True
+
+  def forward(self, x):
+    self.x = x
+    self.y = np.dot(self.weight, x) + self.bias
+    return self.y
+
+  def dy_dw(self):
+    return self.x.T
+
+class InputLayer:
+  def __init__(self):
+    self.x = None
+    self.y = None
+    self.trainable = False
+
+  def forward(self, x):
+    self.x = self.y = x
+    return self.y
+
+  def dy_dx(self):
+    return self.x
+
+class Sigmoid:
+  def __init__(self):
+    self.x = None
+    self.y = None
+    self.trainable = False
+
+  def forward(self, x):
+    self.x = x
+    self.y = 1 / (np.exp(-x))
+    return self.y
+
+  def dy_dx(self):
+    return (1-self.y)*self.y
