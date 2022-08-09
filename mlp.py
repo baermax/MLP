199ce9b2d2d39220069aa7491dc540690f1d1cf0
Max <58471780+adminatgithub@users.noreply.github.com>
2022-08-09 16:31:04 +0200

    Update mlp.py

--- mlp.py
+++ mlp.py
@@ -8,38 +8,30 @@ class MLP:
       out = l.forward(out)
     return out
 
-  def error_gradient(self, y):
-    return y - self.layers[-1].y
-  
   def error(self, y):
-    return 0.5*(y - self.layers[-1].y)**2
+    return y - self.layers[-1].y
+
 
   def back(self, y, lr):
-    dL_dy = self.error_gradient(y)
+    dL_dy = self.error(y)
     loss = dL_dy
     
     for i in reversed(range(1, len(self.layers))):
+
       if self.layers[i].trainable:
-        
+
         self.layers[i].bias += dL_dy * lr
         dy_dw = self.layers[i].dy_dw()
         dW =  np.dot(dL_dy, dy_dw)
         self.layers[i].weight += dW * lr
         dL_dy = np.dot(self.layers[i].weight.T, dL_dy)
-        
+
       else:
         dL_dy = dL_dy * self.layers[i].dy_dx()
-        
+
       return np.sum(abs(loss)) # returns current loss
-  
-  def validate(self, x_test, y_test): # evaluates the model's performance on unkown data samples
-    score = 0
-    for i in range(len(x_test)):
-      pred = decode(self.forward(x_test[i]))
-      if pred == y_test[i]:
-        score += 1
-    return score/len(x_test)
-  
+
+
   def fit(self, x, y, lr, epochs):
     for e in range(epochs):
       current_loss = 0
@@ -47,9 +39,3 @@ class MLP:
         self.forward(x[i])
         current_loss =+ self.back(y[i], lr)
       print(current_loss)
-      
-      
-  
-
-        
-
